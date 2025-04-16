b41b5cdd99d883de9d67b57f2eb094a685e4f121
mthdht <pharos.labo@gmail.com>
2025-04-16 15:24:39 +0200

    made model and migrations for galleries

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,31 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $matt = User::factory()
+            ->hasGalleries(3)
+            ->create([
+                'name' => 'matt',
+                'email' => 'matt@example.com',
+            ]);
+
+        $max = User::factory()->create([
+            'name' => 'max',
+            'email' => 'max@example.com',
         ]);
+
+        $eric = User::factory()->create([
+            'name' => 'eric',
+            'email' => 'eric@example.com',
+        ]);
+
+        
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'manager']);
+        Role::create(['name' => 'recruiter']);
+        Role::create(['name' => 'candidate']);
+
+        $matt->assignRole('admin');
+        $max->assignRole('recruiter');
+        $eric->assignRole('candidate');
     }
 }
